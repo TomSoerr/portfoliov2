f555356e788c3746179a4af5e947b76c2f1b41e7
TomSoerr <tomix2004@gmail.com>
2024-05-08 15:56:27 +0200

    Update config file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   assetsInclude: ["**/*.md"],
-});
+  base: mode === "production" ? "/portfoliov2/" : "/",
+}));
