e743644f333280558d2fb2e4332b1d79a5e4e364
Tom Stöhrer <tomix2004@gmail.com>
2024-05-08 15:17:35 +0200

    Add sidebar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,65 +4,68 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <footer className="section bg">
-      <div className="section-content">
+    <footer className="section-outer">
+      <div className="section bg">
+        <div className="section-content">
 
-        <img src="/img/logo-dark.svg" alt="Logo" />
-        <p className="about">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
-          maiores cum dolorem neque ullam aperiam, ut tempore molestias? Nobis
-          possimus molestias eligendi voluptates id rerum!
-        </p>
+          <img src="/img/logo-dark.svg" alt="Logo" />
+          <p className="about">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
+            maiores cum dolorem neque ullam aperiam, ut tempore molestias? Nobis
+            possimus molestias eligendi voluptates id rerum!
+          </p>
 
-        <div className="social">
-          <h3>Socials</h3>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/ueber-mich">Über mich</Link>
-            </li>
-            <li>
-              <Link to="/projekte">Projekte</Link>
-            </li>
+          <div className="social">
+            <h3>Socials</h3>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/ueber-mich">Über mich</Link>
+              </li>
+              <li>
+                <Link to="/projekte">Projekte</Link>
+              </li>
 
-          </ul>
-        </div>
+            </ul>
+          </div>
 
-        <div className="blog">
-          <h3>Blog</h3>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/ueber-mich">Über mich</Link>
-            </li>
-            <li>
-              <Link to="/projekte">Projekte</Link>
-            </li>
-          </ul>
-        </div>
+          <div className="blog">
+            <h3>Blog</h3>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/ueber-mich">Über mich</Link>
+              </li>
+              <li>
+                <Link to="/projekte">Projekte</Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className="sitemap">
-          <h3>Sitemap</h3>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/ueber-mich">Über mich</Link>
-            </li>
-            <li>
-              <Link to="/projekte">Projekte</Link>
-            </li>
-            <li>
-              <Link to="/kontakt">Kontakt</Link>
-            </li>
-          </ul>
+          <div className="sitemap">
+            <h3>Sitemap</h3>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/ueber-mich">Über mich</Link>
+              </li>
+              <li>
+                <Link to="/projekte">Projekte</Link>
+              </li>
+              <li>
+                <Link to="/kontakt">Kontakt</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
     </footer>
   );
 }
--- src/projekte/tsv-bargteheide.md
+++ src/projekte/tsv-bargteheide.md
@@ -1,1 +1,4 @@
 
+# TSV Bargteheide
+
+Der TSV Bargteheide ist ein Sportverein in Bargteheide. Der Verein wurde 1862 gegründet und hat heute über 3.000 Mitglieder. Der TSV Bargteheide bietet eine Vielzahl von Sportarten an, darunter Fußball, Handball, Volleyball, Basketball, Tennis, Tischtennis, Leichtathletik, Turnen, Gymnastik, Badminton, Schwimmen und Tanzen.
