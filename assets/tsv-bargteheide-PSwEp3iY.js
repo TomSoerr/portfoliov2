19c935ecf8b063ac93304bd11420bdcaf99af1eb
TomSoerr <TomSoerr@users.noreply.github.com>
2024-05-08 18:20:12 +0000

    deploy: f77a2513e1273f10bfb17c2744df38f42e79c849

---
+++ assets/tsv-bargteheide-PSwEp3iY.js
@@ -0,0 +1,4 @@
+const e=`
+# TSV Bargteheide
+
+Der TSV Bargteheide ist ein Sportverein in Bargteheide. Der Verein wurde 1862 gegründet und hat heute über 3.000 Mitglieder. Der TSV Bargteheide bietet eine Vielzahl von Sportarten an, darunter Fußball, Handball, Volleyball, Basketball, Tennis, Tischtennis, Leichtathletik, Turnen, Gymnastik, Badminton, Schwimmen und Tanzen.`;export{e as default};
