686a641f0f7b98e78382225855c3901c3472b4fb
Tom Stöhrer <tomix2004@gmail.com>
2024-05-08 16:44:47 +0200

    Update config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   assetsInclude: ["**/*.md"],
-  base: mode === "production" ? "https://tomsoerr.github.io/portfoliov2/" : "/",
+  base: mode === "production" ? "./" : "/",
 }));
